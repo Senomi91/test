121f82cacd91295de35b4eb8540e9268bd6b13f3
Simone91 <simo.villanini91@gmail.com>
2025-04-24 10:02:42 +0200

    Aggiunti backend e frontend nella repo principale

---
+++ backend/src/api/articolo/controllers/articolo.ts
@@ -0,0 +1,7 @@
+/**
+ * articolo controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::articolo.articolo');
---
+++ backend/src/api/articolo/routes/articolo.ts
@@ -0,0 +1,7 @@
+/**
+ * articolo router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::articolo.articolo');
